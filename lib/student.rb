92b94451639a6f13a9211fc2202f82e345696fb5
fusion407 <fusion407@gmail.com>
2023-02-01 16:26:11 -0600

    commit tree 3223cb6fb1d25c316ed8cd338f9d60e885421219

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,11 @@
-class Student 
-
+class Student < User
+    def initialize
+        @knowledge = []
+    end
+    def learn(knowledge)
+        @knowledge.push(knowledge)
+    end
+    def knowledge
+        return @knowledge
+    end
 end
